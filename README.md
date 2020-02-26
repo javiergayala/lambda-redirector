0c9d0259495d30bfc27d4036c8dc825fecad63ef
Javier Ayala <jayala@rackspace.com>
2020-02-26 14:06:42 -0500

    feat(import): complete the import function

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # lambda-redirector
+
 AWS Lambda function to accept traffic from an ALB, lookup redirects from DynamoDB and return the result.
+
+## DynamoDB Table
+
+The Lambda function expects there to be a DynamoDB Table available for use. The general schema for the records should be:
+
+```json
+{
+  "Site": {
+    "S": "blog.domain.com"
+  },
+  "URI": {
+    "S": "/index2.html"
+  },
+  "RedirectLocation": {
+    "S": "https://www.domain2.com/index2.html"
+  },
+  "HitCount": {
+    "N": "0"
+  },
+  "LastHit": {
+    "S": "0000-00-00 00:00:00"
+  }
+}
+```
+
+## Importing Data
+
+The `lambda_redirect_importer.py` file can be used for a Lambda Function that is triggered whenever a particular S3 location is updated with a new/refreshed file. It can be configured to process a CSV file with the following format:
+
+```csv
+# Site,URI,RedirectLocation
+blog.domain.com,/index2.html,https://www.domain2.com/index2.html
+```
+
+### Setting up `lambda_redirect_importer.py`
+
+#### Requirements
+
+- A Dynamo DB Table setup with for records with the schema mentioned above
+- An S3 Bucket to store the files that will need to be imported
+- A Lambda function to host this script, that will be triggered by the above S3 Bucket
+
+#### Environment Variables
+
+When you setup this lambda function, you need to define the following Environment Variables:
+
+- `DYNAMO_DB_ARN` - The ARN of the DynamoDB instance holding the redirect data
+- `DYNAMO_DB_TABLE` - The name of the DynamoDB table
+- `S3_BUCKET_ARN` - The ARN of the S3 bucket that will be triggering the function
+- `DEBUG` (Optional) - Set to "True" if you want debug info printed to CloudWatch
--- lambda_redirect_importer.py
+++ lambda_redirect_importer.py
@@ -1,12 +1,12 @@
 """Imports new redirects from a CSV file into DynamoDB."""
+import csv
 import os
 import sys
-
-# import json
 
 import boto3
 from botocore.exceptions import ClientError
 from redirect_utils import str2bool
+
 
 """
 This function receives a trigger from S3 that a new CSV file is available in the S3
@@ -24,6 +24,7 @@ Configure these environment variables in your Lambda environment:
 DYNAMO_DB_ARN = os.environ["DYNAMO_DB_ARN"]
 DYNAMO_DB_TABLE = os.environ["DYNAMO_DB_TABLE"]
 S3_BUCKET_ARN = os.environ["S3_BUCKET_ARN"]
+FIELDNAMES = ("site", "from_uri", "redirect_to")
 
 if "DEBUG" in os.environ:
     DEBUG = str2bool(os.environ["DEBUG"])
@@ -43,6 +44,48 @@ except Exception as e:
     sys.exit(1)
 
 
+def readCsv(csvData=None):
+    """Read CSV Data."""
+    print("===== BEGIN readCsv() =====")
+    if not csvData:
+        print("ERROR: NO CSV DATA")
+        return False
+    csvReader = csv.DictReader(csvData, fieldnames=FIELDNAMES)
+    injectResults = []
+    for row in csvReader:
+        injectResults.append(injectRecord(row))
+    print("===== END readCsv() =====")
+    return injectResults
+
+
+def injectRecord(row=[]):
+    """Inject a record into DynamoDB."""
+    if DEBUG:
+        print("===== injectRecord() DEBUG BEGIN =====")
+        print("site: %s" % row["site"])
+        print("from_uri: %s" % row["from_uri"])
+        print("redirect_to: %s" % row["redirect_to"])
+    from_uri_sanitized = row["from_uri"].rstrip("/")
+    try:
+        update_response = ddb.update_item(
+            TableName=DYNAMO_DB_TABLE,
+            Key={
+                "Site": {"S": ("%s" % row["site"])},
+                "URI": {"S": ("%s" % from_uri_sanitized)},
+            },
+            UpdateExpression="SET RedirectLocation = :l",
+            ExpressionAttributeValues={":l": {"S": ("%s" % row["redirect_to"])},},
+        )
+    except ClientError as e:
+        if DEBUG:
+            print("injectRecord ERROR: %s" % e.response["Error"]["Message"])
+        return False
+    else:
+        if DEBUG:
+            print("'response' from DynamoDB: %s" % update_response)
+        return update_response
+
+
 def importFile(record=None):
     """Import file into DynamoDB.
 
@@ -52,26 +95,30 @@ def importFile(record=None):
     Returns:
         [type] -- [description]
     """
-    if DEBUG:
-        print(record)
     s3Bucket = record["bucket"]
     s3ObjectKey = record["object"]["key"]
+    importResult = None
     if DEBUG:
-        print("--- BEGIN importFile() ---")
+        print("===== BEGIN importFile() =====")
         print("S3 Bucket: %s" % s3Bucket)
         print("S3 ObjectKey: %s" % s3ObjectKey)
     if s3Bucket["arn"] != S3_BUCKET_ARN:
         print("SKIPPING %s! WRONG S3 BUCKET!!!" % s3ObjectKey)
+        print("===== ABORT importFile() =====")
         return False
     try:
         s3File = s3.get_object(Bucket=s3Bucket["name"], Key=s3ObjectKey)
     except Exception as e:
         print("ERROR IMPORTING %s: %s" % (s3ObjectKey, e))
+        print("===== ABORT importFile() =====")
         return False
 
-    print(s3File["Body"].read())
+    # csvRaw = s3File["Body"].read().decode()
+    importResult = readCsv(s3File["Body"].read().decode("utf-8").split("\n"))
+    if DEBUG:
+        print("===== END importFile() =====")
 
-    return True
+    return importResult
 
 
 def lambda_handler(event, context):
@@ -82,6 +129,23 @@ def lambda_handler(event, context):
         context {obj} -- context of event that was invoked
 
     """
+    if DEBUG:
+        print("===== BEGIN lambda_handler() =====")
+        print("event['Records']: %s" % event["Records"])
+    finalResult = []
+    filesProcessed = []
     s3Records = event["Records"]
     for record in s3Records:
-        importFile(record["s3"])
+        filesProcessed.append(
+            "%s/%s" % (record["s3"]["bucket"]["arn"], record["s3"]["object"]["key"])
+        )
+        finalResult = finalResult + importFile(record["s3"])
+    if DEBUG:
+        print("FINAL RESULT: %s" % finalResult)
+        print("NUM OF IMPORTED RECORDS: %s" % len(finalResult))
+        print("===== END lambda_handler() =====")
+    finalMsg = {
+        "NumRecordsImported": len(finalResult),
+        "FilesProcessed": filesProcessed,
+    }
+    return finalMsg
